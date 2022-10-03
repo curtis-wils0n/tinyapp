dffd7f3aa4af5bba2a8d0114b7f53787ea3463c0
curtis-wils0n <curtis.wils0n@protonmail.com>
2022-10-03 22:45:30 +0000

    Created new url page and randString function

--- express_server.js
+++ express_server.js
@@ -9,8 +9,14 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com" 
 };
 
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
   res.send('Hello!');
+});
+
+app.get('/hello', (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 app.get('/urls', (req, res) => {
@@ -18,16 +24,33 @@ app.get('/urls', (req, res) => {
   res.render('urls_index', templateVars);
 });
 
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
+});
+
 app.get('/urls/:id', (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render('urls_show', templateVars);
 });
 
-app.get('/hello', (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
-
-
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+app.post('/urls', (req, res) => {
+  console.log(req.body);
+  res.send('OK');
+});
+
+const randString = function() {
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let result = '';
+  for (let i = 0; i < 6; i++) {
+    if (Math.random() > 0.5) {
+      result += chars.charAt(Math.floor(Math.random() * chars.length)).toLowerCase();
+    } else {
+      result += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+  }
+  return result;
+}
