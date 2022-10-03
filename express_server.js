b5c8e847f91632d6915c1f5a86642e3c4b17d9ae
curtis-wils0n <curtis.wils0n@protonmail.com>
2022-10-03 23:17:01 +0000

    Completed URL shortening and link follow-through.

--- express_server.js
+++ express_server.js
@@ -33,13 +33,19 @@ app.get('/urls/:id', (req, res) => {
   res.render('urls_show', templateVars);
 });
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+app.get('/u/:id', (req, res) => {
+  const longURL = urlDatabase[req.params.id];
+  res.redirect(longURL);
 });
 
 app.post('/urls', (req, res) => {
-  console.log(req.body);
-  res.send('OK');
+  const newID = randString();
+  urlDatabase[newID] = req.body.longURL;
+  res.redirect(`/urls/${newID}`);
+});
+
+app.listen(PORT, () => {
+  
 });
 
 const randString = function() {
